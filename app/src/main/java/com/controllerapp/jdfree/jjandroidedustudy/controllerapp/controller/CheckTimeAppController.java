ff548d4d78312d9db7da7b2afe1ceb3f0c3a3cc0
jaehoonLee <ljhgood9925@gmail.com>
2018-10-28 07:06:23 +0900

    리모델링 전

--- app/src/main/java/com/controllerapp/jdfree/jjandroidedustudy/controllerapp/controller/CheckTimeAppController.java
+++ app/src/main/java/com/controllerapp/jdfree/jjandroidedustudy/controllerapp/controller/CheckTimeAppController.java
@@ -12,15 +12,19 @@ import com.controllerapp.jdfree.jjandroidedustudy.controllerapp.R;
 import com.controllerapp.jdfree.jjandroidedustudy.controllerapp.activity.MainActivity;
 import com.controllerapp.jdfree.jjandroidedustudy.controllerapp.model.AppListModel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CheckTimeAppController extends Service implements Runnable {
 
-    private boolean isCheck = true;
+    private boolean isCheck;
     private List<AppListModel> mList;
     private MediaPlayer mPlayer;
     private Thread mThread;
+    public static final String TAG = "abcasdf";
 
     @Override
     public void onCreate() {
@@ -30,14 +34,21 @@ public class CheckTimeAppController extends Service implements Runnable {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-
-        mList = intent.getParcelableArrayListExtra(MainActivity.CHECK_CONTROLLER);
+        if (intent != null) {
+            mList = intent.getParcelableArrayListExtra(MainActivity.CHECK_CONTROLLER);
+            if (mList.get(0).getAppInfo() != null){
+                Log.e(TAG, "onStartCommand: ffffffffffffffffffffffffffffffffffffffffff" );
+            }
+        }
+        isCheck = true;
 
         mThread = new Thread(this);
         mPlayer = MediaPlayer.create(this, R.raw.song);
         mPlayer.setLooping(true);
         mPlayer.start();
         mThread.start();
+
+//        abc();
 
 
 //                putParcelableArrayListExtra
@@ -47,26 +58,142 @@ public class CheckTimeAppController extends Service implements Runnable {
 
     @Override
     public void onDestroy() {
+        Log.e(TAG, "onDestroy: " + "exit");
         isCheck = false;
         mPlayer.stop();
-        mThread.interrupt();
-        mThread = null;
+//        mThread.interrupt();
+//        mThread = null;
         super.onDestroy();
+    }
+
+    private void abc() {
+        try {
+
+            while (isCheck) {
+                Thread.sleep(1000);
+
+                Log.e(TAG, "run: ");
+                if (mList != null) {
+                    if(mList.get(0).getAppInfo() != null){
+                        Log.e(TAG, "abc: fsdfsdfdsf");
+                    }
+                    Log.e(TAG, "run: " + mList.size() + mList.get(5).getAppInfo().packageName);
+                }
+//                Intent intent = new Intent(this, MainActivity.class);
+//                startActivity(intent);
+
+                Intent launchIntent = getPackageManager().getLaunchIntentForPackage("com.package.address");
+                startActivity(launchIntent);
+
+//                Intent i = new Intent();
+//                i.setAction(Intent.ACTION_MAIN);
+//                i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+//                        Intent.FLAG_ACTIVITY_CLEAR_TOP |
+//                        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+//                i.addCategory(Intent.CATEGORY_HOME);
+//                startActivity(i);
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+//            isCheck = false;
+//            mThread.stop();
+//            mThread.destroy();
+//            this.stopSelf();
+        }
     }
 
     @Override
     public void run() {
+        try {
 
+            while (isCheck) {
+                Thread.sleep(1000);
+
+                Log.e(TAG, "run: ");
+                if (mList != null) {
+                    Log.e(TAG, "run: " + mList.size());
+                }
+            }
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            isCheck = false;
+//            mThread.stop();
+//            mThread.destroy();
+//            this.stopSelf();
+        }
+        /*
+        Process process = null;
+
+        try {
+            Runtime.getRuntime().exec("/system/bin/logcat -c");
+
+            process = Runtime.getRuntime().exec("/system/bin/logcat -b main -s ActivityManager:I");
+
+
+        } catch (IOException e) {
+//            e.printStackTrace();
+
+            Log.e(getPackageName(), e.toString());
+        }
+
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+
+
+            while (isCheck) {
+                line = reader.readLine();
+                Log.e("dd", "run: " + "ffffffffffffffffffffffffffff" + line);
+
+//                for (int i = 0; i < mList.size(); i++) {
+//                    Log.e("dd", "run: " + line);
+
+//                }
+            }
+
+        } catch (IOException e) {
+            Log.e(getPackageName(), e.toString());
+        } finally {
+            try {
+                reader.close();
+            } catch (IOException e) {
+                Log.e(getPackageName(), e.toString());
+            }
+
+            process.destroy();
+            process = null;
+            mThread.stop();
+            mThread.destroy();
+            reader = null;
+            this.stopSelf();
+        }
+
+        */
+        /*
         while (isCheck) {
+
             if (mList == null) {
 //                Toast.makeText(this, "mList 받는 값 없음.", Toast.LENGTH_SHORT).show();
 //                break;
             }
 
             for (int i = 0; i < mList.size(); i++) {
-//                Log.e("fff", "run: " + mList.get(i));
+                Log.e("fff", "run: " + mList.get(i));
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
+        */
     }
 
     @Override
