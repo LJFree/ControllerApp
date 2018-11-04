2c3d8705f49a59e7e27ef6a2a61d1359a52ceb02
jaehoonLee <ljhgood9925@gmail.com>
2018-11-04 23:02:00 +0900

    최종 테스트 2

--- app/src/main/java/com/controllerapp/jdfree/jjandroidedustudy/controllerapp/activity/MainActivity.java
+++ app/src/main/java/com/controllerapp/jdfree/jjandroidedustudy/controllerapp/activity/MainActivity.java
@@ -74,10 +74,6 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
 
         mAdapter.onSetClicked(this);    // 리사이클러뷰 이벤트
 
-//        if (mAppList.size() != 0) {
-//            goService();
-//        }
-
     }
 
     // ICon 클릭 이벤트(Data 부분)
@@ -189,9 +185,9 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
     @Override
     protected void onStart() {
         super.onStart();
+
         // bind 서비스 시작
         bindService(mIntent, mConnection, BIND_AUTO_CREATE);
-        listSave(); // 데이터 저장
     }
 
     // 화면이 안보일 때 이벤트
@@ -280,7 +276,7 @@ public class MainActivity extends AppCompatActivity implements MainRecyclerViewA
     // 종료 이벤트
     @Override
     protected void onDestroy() {
-//        listSave();
+        listSave();
         super.onDestroy();
     }
 
